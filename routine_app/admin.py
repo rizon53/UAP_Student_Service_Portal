7c8433cebee042a70a1ac81a251d5efcf57c41e1
AbirOPS2 <23101146@uap-bd.edu>
2025-10-14 17:27:12 +0600

    Routine app: added forms.py and  admin.py

--- routine_app/admin.py
+++ routine_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Routine
+
+class RoutineAdmin(admin.ModelAdmin):
+    list_display = ('title', 'department', 'year', 'routine_file')
+
+admin.site.register(Routine, RoutineAdmin)
--- routine_app/views.py
+++ routine_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Routine
+
+def routine_list(request):
+    routines = None
+    department = request.GET.get('department')
+    year = request.GET.get('year')
+
+    if department and year:
+        routines = Routine.objects.filter(department=department, year=year)
+
+    context = {
+        'routines': routines,
+        'departments': Routine._meta.get_field('department').choices,
+        'years': Routine._meta.get_field('year').choices,
+        'selected_dept': department,
+        'selected_year': year,
+    }
+    return render(request, 'routine_list.html', context)
